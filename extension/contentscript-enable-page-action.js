072ccb81ab1d9b58fbbbff5c9d0b82ba3ef14a16
Filip Wasilewski <en@ig.ma>
2011-11-25 15:47:30 +0100

    Added contentscript for initiating page action.

---
+++ extension/contentscript-enable-page-action.js
@@ -0,0 +1,6 @@
+/* Copyright Filip Wasilewski <en@ig.ma>. All rights reserved.
+ * Open Source software available under the MIT license terms.
+ * More info at http://en.ig.ma/notebook/2011/chrome-extension-for-ai-class-forum
+ */
+
+chrome.extension.sendRequest({"action": "init"}, function(response) {});
